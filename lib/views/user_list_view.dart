614c85705e71c27be3707d1de39dfef568ebeee5
maimand <manmaihuu@gmail.com>
2021-05-14 19:13:58 +0700

    Fix refresh function for load user list

--- lib/views/user_list_view.dart
+++ lib/views/user_list_view.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:door_opener/model/user.dart';
+import 'package:door_opener/services/service.dart';
 import 'package:door_opener/views/add_user_view.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
@@ -26,31 +28,42 @@ class _UserListState extends State<UserList> {
 
   Future getList() async {
     print('loading user list');
-    await widget.databaseReference
-        .child("door-opener-a3c06-default-rtdb/user")
-        .once()
-        .then((DataSnapshot dataSnapshot) {
-      Map<dynamic, dynamic> values = dataSnapshot.value;
-      values.forEach((key, values) {
-        users.add(new User(name: values["name"], data: values["data"]));
-        setState(() {});
+    try {
+      await widget.databaseReference
+          .child("door-opener-a3c06-default-rtdb/user")
+          .once()
+          .then((DataSnapshot dataSnapshot) {
+        Map<dynamic, dynamic> values = dataSnapshot.value;
+        values.forEach((key, values) {
+          users.add(new User(name: values["name"], data: values["data"]));
+        });
       });
-    });
+      print('loaded');
+      setState(() {
+      });
+    } catch (e) {
+      // TODO
+      print(e);
+    }
   }
 
-  Future saveUser(String name, String data) async {
+  Future saveUser(String name, File file) async {
     try {
-      await widget.databaseReference.child("door-opener-a3c06-default-rtdb/user").set({
-        'name': name,
-        'data': data
-      });
-      users.add(new User(name: name, data: data));
+      await Service.createUser(name, file);
+      final bytes = file.readAsBytesSync();
+      String img64 = base64Encode(bytes);
+      users.add(new User(name: name, data: img64));
       setState(() {
       });
     } catch (e) {
       // TODO : add snack bar to show toast
       print(e);
     }
+  }
+
+  Future refreshUserList() async {
+    users = new List();
+    await getList();
   }
 
   @override
@@ -97,7 +110,7 @@ class _UserListState extends State<UserList> {
       )),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.refresh),
-        onPressed: getList,
+        onPressed: refreshUserList,
       ),
     );
   }
